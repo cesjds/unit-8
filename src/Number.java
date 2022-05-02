0116cd65a4e28c95da2ca8459324aa696b1813c0
Yoav Amit <yoavamit@icloud.com>
2022-05-02 09:38:10 -0400

    Inital completetion Number class

--- src/Number.java
+++ src/Number.java
@@ -20,20 +20,52 @@ public class Number {
         this.num = num;
     }
 
-    public int getHexadecimal(int num) {
+    public String getHexadecimal(int num) {
+        int rem = 0;
+        String hexaNum = "";
+
+        // the hexadecimal system
         char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-        int rem = num % 16;
-        String str= hex[rem] + str;
-        num = num / 16;
-        return num;
+        while(num > 0)
+        {
+            rem = num % 16;
+            hexaNum = hex[rem] + hexaNum;
+            num = num / 16;
+        }
+
+        return hexaNum;
     }
 
-    public int getOctal(int num) {
-        return Integer.toOctalString(num);
+    public String getOctal(int num) {
+        int rem = 0;
+        int octaNum = 0;
+        int i = 1;
+
+        while(num > 0){
+            rem = num%8;
+            octaNum +=  i * rem;
+            num = num/8;
+            i = i * 10;
+        }
+
+        return Integer.toString(octaNum);
     }
 
-    public String getBinary
+    public String getBinary(int num) {
+        int binary[] = new int[40];
+        int index = 0;
+        String binaNum = "";
 
+        while(num > 0) {
+            binary[index ++] = num % 2;
+            num = num / 2;
+        }
+        for(int i = index - 1; i >= 0; i--) {
+            System.out.print(binary[i]);
+            binaNum += binary[i];
+        }
 
+        return binaNum;
+    }
 }
